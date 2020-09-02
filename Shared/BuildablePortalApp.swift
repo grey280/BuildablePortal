c551bee7e2d31f143a02b8dc0734a87ff444f5b0
grey280 <greypatterson@gmail.com>
2020-09-02 15:54:53 -0700

    Initial Commit

---
+++ Shared/BuildablePortalApp.swift
@@ -0,0 +1,17 @@
+//
+//  BuildablePortalApp.swift
+//  Shared
+//
+//  Created by Grey Patterson on 9/2/20.
+//
+
+import SwiftUI
+
+@main
+struct BuildablePortalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Shared/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Shared
+//
+//  Created by Grey Patterson on 9/2/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
