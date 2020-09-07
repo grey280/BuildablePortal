02b2a31f2fad768415da786feff806c5fb93bc27
grey280 <greypatterson@gmail.com>
2020-09-07 12:30:32 -0700

    CacheService

--- Shared/Helpers/NetworkHelpers.swift
+++ Shared/Helpers/NetworkHelpers.swift
@@ -230,53 +230,6 @@ struct DefaultStorage {
     @UserDefault("signin_token", defaultValue: "") var token: String
 }
 
-class CacheService: ObservableObject {
-    public static var shared = CacheService()
-    private init(){
-        self.reloadCacheAll()
-        // allows auto-refresh when signin happens
-        signinSubscription = AuthService.shared.objectWillChange.print("objectWillChange").sink(receiveValue: { (_) in
-            if (AuthService.shared.loggedIn){
-                self.reloadCacheAll()
-            }
-        })
-    }
-    
-    private var signinSubscription: AnyCancellable!
-    private var subscriptions: [AnyCancellable] = []
-    
-    func reloadCacheAll(){
-        print("reloadCacheAll()")
-        for sub in subscriptions{
-            sub.cancel()
-        }
-        let accounts = Network.getResultItems(nil, route: URL(string: "https://portal.buildableworks.com/api/Account/Accounts/getResultItems")!)
-            .print("reloadCacheAll.accounts")
-            .replaceError(with: [])
-            .assign(to: \.cachedAccounts, on: self)
-        let activities = Network.getResultItems(nil, route: URL(string: "https://portal.buildableworks.com/api/Finance/TimesheetActivities/getResultItems")!)
-            .print("reloadCacheAll.activities")
-            .replaceError(with: [])
-            .assign(to: \.cachedActivities, on: self)
-        let options = SearchOptions()
-        options.pagingDisabled = true
-        let accountProjects = Network.getItems(options, route: URL(string: "https://portal.buildableworks.com/api/Account/AccountProjects/getItems")!)
-            .print("reloadCacheAll.accountProjects")
-            .replaceError(with: [])
-            .assign(to: \.cachedAccountProjects, on: self)
-        subscriptions = [accounts, activities, accountProjects]
-    }
-    
-    @Published private(set) var cachedAccounts: [ListResultItem] = []
-    @Published private(set) var cachedActivities: [ListResultItem] = []
-    
-    @Published private(set) var cachedAccountProjects: [AccountProject] = []
-    
-    
-    
-    
-}
-
 enum NetworkError: Error {
     // Errors occurring in non-network level
     case unknown, unableToDecode, unableToEncode
